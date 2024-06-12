5a828310151d75b2a9b8cfe3ad0ea41e2d35dbf5
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2024-06-12 12:18:41 +0700

    first commit

---
+++ config/products.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'name' => 'Baju 1',
+        'price' => 100000,
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        'image' => 'https://res.cloudinary.com/dvrhf8d9t/image/upload/v1718041291/integratif/Untitled_igwobu.jpg'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Baju 2',
+        'price' => 599000,
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        'image' => 'https://res.cloudinary.com/dvrhf8d9t/image/upload/v1718041289/integratif/msm_baju_koko_pendek_4_button_am0-58520-3073_8878-twebp80_hl5yzt.webp'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Baju 3',
+        'price' => 499000,
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        'image' => 'https://res.cloudinary.com/dvrhf8d9t/image/upload/v1718041289/integratif/images1_m0tpld.jpg'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Baju 4',
+        'price' => 99000,
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        'image' => 'https://res.cloudinary.com/dvrhf8d9t/image/upload/v1718041289/integratif/images_pezlcb.jpg'
+    ],
+   
+];
